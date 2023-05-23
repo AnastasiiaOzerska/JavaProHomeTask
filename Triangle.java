a8ea238cc95e6e9a9ff5eecfd968b937f3ff56f5
AnastasiiaOzerska <122531008+AnastasiiaOzerska@users.noreply.github.com>
2023-05-23 18:31:02 +0300

    Delete Triangle.java

--- Triangle.java
+++
@@ -1,9 +0,0 @@
-public class Triangle implements TwoDimensional{
-    int height = 6;
-    int width = 3;
-    @Override
-    public double calcArea(){
-        double areaTriangle = (height * width)/2;
-        return areaTriangle;
-    }
-}
