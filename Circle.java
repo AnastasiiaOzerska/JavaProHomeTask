d1143f3a430c3b88e45ac3974e05947270d0980c
AnastasiiaOzerska <122531008+AnastasiiaOzerska@users.noreply.github.com>
2023-05-23 18:30:18 +0300

    Delete Circle.java

--- Circle.java
+++
@@ -1,9 +0,0 @@
-public class Circle implements TwoDimensional {
-    final double PI = 3.14;
-    int radius = 5;
-    @Override
-    public double calcArea(){
-        double areaCircle = PI * (radius * radius);
-        return areaCircle;
-    }
-}
