babf6afbb07ec04bf1c71c9c1407e7c8734351f8
Ozerska Anastasiia <a.ozolova@gmail.com>
2023-05-26 00:04:43 +0300

    initial commit

---
+++ src/coffe_order/CoffeeOrderBoard.java
@@ -0,0 +1,39 @@
+package coffe_order;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class CoffeeOrderBoard {
+private Queue<Order> holidayOrders;
+private int queueNumber;
+
+    public CoffeeOrderBoard() {
+        this.holidayOrders = new LinkedList<>();
+    }
+    public void add (String name){
+        queueNumber++;
+        holidayOrders.add(new Order(queueNumber,name));
+    }
+    public Order deliver(){
+        Order item = holidayOrders.remove();
+
+        return item;
+    }
+    public Order deliver(int number){
+        for (Order holidayOrder : holidayOrders) {
+            if(holidayOrder.getNumber()==number){
+                holidayOrders.remove(holidayOrder);
+                return holidayOrder;
+            }
+
+        }
+        return null;
+    }
+    public void draw(){
+        System.out.println("Num | Name");
+        for (Order holidayOrder:holidayOrders) {
+            System.out.println(holidayOrder.toString());
+
+        }
+    }
+}
---
+++ src/coffe_order/Main.java
@@ -0,0 +1,27 @@
+package coffe_order;
+
+public class Main {
+    public static void main(String[] args) {
+        CoffeeOrderBoard newYear = new CoffeeOrderBoard();
+        newYear.add("Mary");
+        newYear.add("Bob");
+        newYear.add("Mark");
+        newYear.add("Olga");
+        newYear.add("Alex");
+        newYear.add("Andrew");
+        newYear.add("Anton");
+        newYear.add("Tom");
+        newYear.add("Jerry");
+        newYear.add("Oleg");
+        newYear.add("Pedro");
+        newYear.draw();
+        System.out.println("====================");
+        System.out.println(newYear.deliver(2));
+        System.out.println("===================");
+        System.out.println(newYear.deliver());
+        newYear.draw();
+        newYear.add("Nastya");
+        newYear.draw();
+
+    }
+}
