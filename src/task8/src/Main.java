5ff7811654d1a23eec2b21de8f124b53f434019e
Ozerska Anastasiia <a.ozolova@gmail.com>
2023-05-27 15:32:48 +0300

    initial commit

---
+++ src/task8/src/Main.java
@@ -0,0 +1,16 @@
+package task8.src;
+
+public class Main {
+    public static void main(String[] args) {
+    FileNavigator manager = new FileNavigator();
+    manager.add("D:\\new folder",new FileData("Home",2345,"D:\\new folder"));
+    manager.add("E:\\new folder",new FileData("Cat",2350,"E:\\new folder"));
+    manager.add("C:\\new folder",new FileData("Dog",2348,"C:\\new folder"));
+    manager.add("D:\\new folder",new FileData("Bird",2322,"D:\\new folder"));
+    manager.add("D:\\new folder",new FileData("Mouse",4563,"D:\\new folder"));
+        System.out.println(manager.filterBySize(3000));
+        System.out.println(manager.remove("C:\\new folder"));
+        System.out.println(manager.sortBySize());
+        System.out.println(manager.find("D:\\new folder"));
+    }
+}
